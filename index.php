4e7c7c61910621691f328a54e649b9fef19b0404
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-17 12:30:54 +0300

    feature/sendsms

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+function sendSms($phoneNumber,$message){
+
+    $apiKey="REPLACE_YOUR_OWN KEY_HERE";
+
+    $sendeName="23107";
+
+    $bodyRequest =array(
+        "mobile" =>$phoneNumber,
+        "response_type"=>"json",
+        "sender_name"=>$sendeName,
+        "service_id"=>0,
+        "message"=>$message
+    );
+    $payload = json_encode($bodyRequest);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://api.mobitechtechnologies.com/sms/sendsms',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 15,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>$payload,
+    CURLOPT_HTTPHEADER => array(
+        'h_api_key: '.$apiKey,
+        'Content-Type: application/json'
+    ),
+    ));
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+    echo $response;
+
+}
+
+sendSms("254757177738",
+"This is a message.\n\nRegards\nMobiTech Technologies");
+
+?>
